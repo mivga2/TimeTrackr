71ef8bd80ab4920dcfa9e1a69e21ce2df135e95d
mivga2 <gavlak2@uniba.sk>
2024-04-05 21:42:06 +0200

    add test of connecting to database and calling api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,44 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  connectionString: process.env.DBConnLink,
+  ssl: {
+      rejectUnauthorized: false
+  }
+});
+
+const getUsers = async (request, response) => {
+   const res = await pool.query('SELECT * FROM public."Users"', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+    console.log(res)
+  })
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello from our server!')
 })
 
+app.get('/users', getUsers)
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App running on port ${port}.`)
 })
--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,8 +1,18 @@
 import Card from "../components/Card";
+import axios from 'axios';
+
+const apiCall = () => {
+  axios.get('http://localhost:3000').then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data)
+  })
+}
 
 const Overview = () => {
+
   return (
     <div>
+      <button onClick={apiCall}>Make API Call</button>
       <Card title="Calendars" />
       <Card title="Events" />
       <Card title="Tasks" />
