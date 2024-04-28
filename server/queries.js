7d25d139b77502f6a637794a698e9d921210756d
mivga2 <gavlak2@uniba.sk>
2024-04-28 22:57:32 +0200

    add list of events for calendar query

--- server/queries.js
+++ server/queries.js
@@ -372,7 +372,20 @@ export const postFriendRequest = async (request, response) => {
 // calendar queries
 export const getAllEventsByCalId = async (request, response) => {
   const sql =
-    'SELECT u.id, u.username FROM public."Friend_requests" AS fr, public."Users" AS u WHERE fr.receive_user_id = $1 AND fr.accepted = false AND u.id = fr.send_user_id';
+    'SELECT e.date_from, e.date_to, e.id, e.color, e.name FROM public."Events" AS e, public."Calendar_permissions" AS cp WHERE e.calendar_id = cp.calendar_id AND cp.calendar_id = $2 AND cp.user_id = $1';
+  const values = ["3ab413ce-21ad-4868-9f11-c7b24e041b47", request.params.calendar_id];
+  const res = await pool.query(sql, values, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+    console.log(res);
+  });
+};
+
+export const getAllOwnedCalendars = async (request, response) => {
+  const sql =
+    'SELECT calendar_id, name FROM public."Calendar_permissions" AS cp, public."Calendars" AS c WHERE c.id = cp.calendar_id AND cp.user_id = $1 AND cp.owner = true';
   const values = ["3ab413ce-21ad-4868-9f11-c7b24e041b47"];
   const res = await pool.query(sql, values, (error, results) => {
     if (error) {
